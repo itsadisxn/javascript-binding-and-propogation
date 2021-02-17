63ade1a17e365bba4e835e5271f6bc2d10c893bf
itsadisxn <55608486+itsadisxn@users.noreply.github.com>
2021-02-17 01:48:50 -0500

    basic project structure

---
+++ js/main.js
@@ -0,0 +1,7 @@
+// Building a JavaScript IFFE Function ( Immediately Invoked Function Expression ) 
+// IFFE is a JavaScript function that runs as soon as it is defined.
+(function () {
+    var Game = function (el, option) {
+
+    }
+})();
